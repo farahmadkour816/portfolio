a704a46ae20d3fb953b15c221c1fd9903389811d
Farah Madkour <farah.madkour@degservices.ca>
2025-07-04 21:15:08 -0400

    Fix Vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/portfolio/", // 👈 this MUST match your repo name
+   base: '/', // 👈 this MUST match your repo name
   plugins: [react()],
 });
